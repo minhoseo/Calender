110f5e23534710b28a22630492306c550e2f6062
jacks <jacks@DESKTOP-AKG8EQ4>
2018-01-02 10:37:59 +0900

    update Calender.

--- src/Sum.java
+++ src/Sum.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Sum {
 	public static void main(String[] args) {
-		System.out.println("int");
+		int a,b;
+		System.out.println("두 수를 입력해주세요.: ");
+		Scanner scanner=new Scanner(System.in);
+		a=scanner.nextInt();
+		b=scanner.nextInt();
+		System.out.println(a+b);
+		scanner.close();
 	}
 }
